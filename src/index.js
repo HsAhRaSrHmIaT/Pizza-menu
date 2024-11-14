a31af338d121fddda0791c606582b65944bb59f1
Harshit Sharma <123459218+HsAhRaSrHmIaT@users.noreply.github.com>
2024-11-14 17:21:36 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,126 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+
+  return (
+  <div className="container">
+    {/* <h1>I am nobody</h1> */}
+    <Header />
+    <Menu />
+    <Footer />
+  </div>
+  );
+}
+
+function Header() {
+  const styles = {
+  };
+  return (
+    <header className="header">
+      <h1 style={styles}>My Pizza Place</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  const pizzas = pizzaData.map((pizza) => (
+    <Pizza
+      name={pizza.name}
+      ingredients={pizza.ingredients}
+      price={pizza.price}
+      photoName={pizza.photoName}
+      soldOut={pizza.soldOut}
+    />
+  ));
+  return (
+    <main className="menu">
+      <h2>Menu</h2>
+      <ul>
+        {pizzas}
+      </ul>
+    </main>
+  );
+}
+function Footer() {
+  // { 
+    // const hour = new Date().getHours();
+    // const openHour = 9;
+    // const closeHour = 22;
+    // const isOpen = hour >= openHour && hour <= closeHour;
+
+  //   if (hour >= openHour && hour <= closeHour) {
+  //     alert('We are open');
+  //   }
+  // }
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}.
+      We are open 24/7.
+    </footer>
+  );
+}
+
+function Pizza(props) {
+  return (
+  <div className="pizza">
+    <img src={props.photoName} alt={props.name} />
+    <div>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p> 
+      <span>{props.price}</span>
+    </div>
+  </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+root.render(<React.StrictMode>
     <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </React.StrictMode>);
